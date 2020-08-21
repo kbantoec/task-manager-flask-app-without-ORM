28892cd26248938ad23ff6f380a4bf46354750dc
Kai Banto Eckert <y.banto@outlook.com>
2020-08-21 11:31:21 +0200

    Added the utils module

--- tmapp/__init__.py
+++ tmapp/__init__.py
@@ -3,7 +3,4 @@ from .views import app
 # Tell Flask that we are working with a database
 from . import models
 
-# Connect our app to the database
-@app.cli.command()
-def test_db():
-    models.test_db()
+from . import utils
